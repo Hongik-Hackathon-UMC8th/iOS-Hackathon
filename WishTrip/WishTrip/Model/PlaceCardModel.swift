2b0ef3618b927798aaa2f7d7c97f014ac3f78c23
jiwookim1202 <godblessjiwoo@gmail.com>
2025-06-22 15:47:30 +0900

    HomeView 최종 완성 (#7)
    * HomeView 완성
    
    * HomeView 최종

---
+++ WishTrip/WishTrip/Model/PlaceCardModel.swift
@@ -0,0 +1,14 @@
+//
+//  PlaceCardModel.swift
+//  WishTrip
+//
+//  Created by 김지우 on 6/22/25.
+//
+
+
+import Foundation
+
+struct PlaceCardModel: Identifiable, Hashable {
+    let id = UUID()
+    var imageName: String
+}
